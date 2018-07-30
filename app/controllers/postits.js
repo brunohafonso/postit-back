700169c21f6aaea4ec0358a27553aa61a9b1fb1a
brunohafonso <brunohafonso@gmail.com>
2018-07-29 22:52:46 -0300

    crud postit ok

--- app/controllers/postits.js
+++ app/controllers/postits.js
@@ -1,18 +1,71 @@
 module.exports = (app) => {
-    app.get('/boards/postits', (req, res) => {
+    app.get('/postits', (req, res) => {
         const connection = app.model.connectionFactory();
         const postitsDAO = new app.model.PostitsDAO(connection);
-        postitsDAO.listar((error, results) => res.json(results));
+        postitsDAO.listar((error, results) => {
+            if(error) {
+                res.status(500).json('erro inesperado: ' + error);
+            }
+            res.status(200).json(results);
+        });
         connection.end(); 
     });
 
-    app.post('/boards/postits', (req, res) => {
+    app.post('/postits', (req, res) => {
         const postit = req.body;
+        req.assert('content', 'content é obrigatório').notEmpty();
+        req.assert('idBoard', 'idBoard é obrigatório').notEmpty();
+        req.assert('positionTop', 'positionTop é obrigatório').notEmpty();
+        req.assert('positionLeft', 'positionLeft é obrigatório').notEmpty();
+        req.assert('color', 'color é obrigatório').notEmpty();
+        const erros = req.validationErrors();
+        if(erros) {
+            res.status(400).json('erros ao cadastrar board ' + erros[0].msg);
+        }
         const connection = app.model.connectionFactory();
         const postitsDAO = new app.model.PostitsDAO(connection);
         postitsDAO.salvar(postit, (error, results) => {
-            res.json(postit)
+            if(error) {
+                res.status(500).json('erro inesperado: ' + error);
+            }
+            res.status(200).json('postit salvo com sucesso.');
         });
         connection.end(); 
+    });
+
+    app.put('/postits/:id', (req, res) => {
+        const id = req.params.id;
+        const postit = req.body;
+        req.assert('content', 'content é obrigatório').notEmpty();
+        req.assert('idBoard', 'idBoard é obrigatório').notEmpty();
+        req.assert('positionTop', 'positionTop é obrigatório').notEmpty();
+        req.assert('positionLeft', 'positionLeft é obrigatório').notEmpty();
+        req.assert('color', 'color é obrigatório').notEmpty();
+        const erros = req.validationErrors();
+        if(erros) {
+            res.status(400).json('erros ao cadastrar board ' + erros[0].msg);
+        }
+        const connection = app.model.connectionFactory();
+        const postitsDAO = new app.model.PostitsDAO(connection);
+        postitsDAO.atualizar(id, postit, (error, result) => {
+            if(error) {
+                res.status(500).json('erro inesperado: ' + error);
+            }
+            res.status(200).json('postit atualizado com sucesso.');
+        });
+        connection.end();
+    });
+
+    app.delete('/boards/:id', (req, res) => {
+        const id = req.params.id;
+        const connection = app.model.connectionFactory();
+        const postitsDAO = new app.model.PostitsDAO(connection);
+        postitsDAO.deletar(id, (error, results) => {
+            if(error) {
+                res.status(500).json('erro inesperado: ' + error);
+            }
+            res.status(200).json('postit deletado com sucesso');
+        });
+        connection.end();
     });
 }
--- app/model/PostitsDAO.js
+++ app/model/PostitsDAO.js
@@ -10,6 +10,15 @@ PostitsDAO.prototype.salvar = function(postit, callback) {
     this._connection.query('insert into postits set ?', postit, callback);
 }
 
+PostitsDAO.prototype.atualizar = function(id, postit, callback) {
+    this._connection.query(`update postits set ? where id = ${ id }`, postit, callback);
+}
+
+PostitsDAO.prototype.deletar = function(id, callback) {
+    this._connection.query(`delete from postits where id = ${ id }`, callback);
+}
+
 module.exports = function(connection) {
     return PostitsDAO;
 }
+1
